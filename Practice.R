3a7570f4d1f1e6f7efe14920d917d0c281f2d250
LifeOfJacobEnto <jbasso@bell.net>
2020-06-25 19:21:32 -0400

    got rid of some shit

--- Practice.R
+++ Practice.R
@@ -200,4 +200,3 @@ OviANOVA=aov(Complete$OvipositionPeriod~Complete$TreatmentName)
 OviTukeyHSD=HSD.test(OviANOVA, trt = 'Complete$TreatmentName', group = TRUE)
 OviTukeyHSD
 
-#this is for my test branch
