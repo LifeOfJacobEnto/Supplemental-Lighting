465ebf4755540929f26833c8289973b29d7f198f
LifeOfJacobEnto <jbasso@bell.net>
2020-06-30 12:35:07 -0400

    Summmary statistics, boxplots, and ANOVA for only Total Development (days)

--- Development/Orius Development.R
+++ Development/Orius Development.R
@@ -35,6 +35,48 @@ summary(Tibia)
 
 # Development Rate Analysis -----------------------------------------------
 
+# measures of spread
+summary(Rate)
+
+# Histograms
+hist(Rate$Total.Development..days.)
+
+# Summary stats (Mean, SD, SE, n)
+# just Total Development (days)
+Rate %>% summarise(Mean = mean(Total.Development..days.), SD = sd(Total.Development..days.), SE = sd(Total.Development..days.)/sqrt(length(Total.Development..days.)), n = length(Total.Development..days.))
+# then grouped by Block and Treatment
+Groupedrate = Rate %>% group_by(Block, Treatment)
+Groupedrate %>% summarise(Mean = mean(Total.Development..days.), SD = sd(Total.Development..days.), SE = sd(Total.Development..days.)/sqrt(length(Total.Development..days.)), n = length(Total.Development..days.))
+#then for All Nymphal Instars NOT WORKING
+Groupedrate %>% summarise(across(c("N1d":"Total.Development..days.")), .funs = (Mean = mean(), SD = sd(), SE = sd()/sqrt()/length(), n = length()))
+
+# Boxplot
+boxplot(Rate$Total.Development..days. ~ Rate$Treatment, xlab = "Treatment", ylab = "Total Development (days)")
+# or using ggplot
+ggplot(Rate, aes(x= Treatment, y= Total.Development..days.)) + 
+  geom_boxplot() +
+  xlab("Treatment") +
+  ylab("Total Development (days)")
+
 # ? make Nymphal Instar Development Time into long format?
 
 # ANOVA
+TotaldevANOVA= aov(Rate$Total.Development..days. ~ Rate$Treatment)
+summary(TotaldevANOVA)
+
+# Tukey HSD
+TotaldevTukeyHSD=HSD.test(TotaldevANOVA, trt = 'Rate$Treatment', group = TRUE)
+TotaldevTukeyHSD
+
+# ? is there a difference between Blocks? ie. use 2-way anova with Block and Treatment
+TotaldevANOVA2 = aov(Rate$Total.Development..days. ~ Rate$Block + Rate$Treatment)
+summary(TotaldevANOVA2)
+TotaldevTukeyHSD2 = HSD.test(TotaldevANOVA2, trt = 'Rate$Block', group = TRUE)
+TotaldevTukeyHSD2
+boxplot(Rate$Total.Development..days. ~ Rate$Block + Rate$Treatment, xlab = "Treatment", ylab = "Total Development (days)")
+# Yes, but this may be due to low sample size and some Treatments not having samples in both Blocks
+
+# Boxplot with Sig. Groups
+
+# Normality and Assumptions
+
