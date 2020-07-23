80064561065cb6cb7ad69c1a0467398d68f011bb
LifeOfJacobEnto <jbasso@bell.net>
2020-07-22 23:08:20 -0400

    Edited Total Development analysis for updated dataset

--- Development/Orius Development.R
+++ Development/Orius Development.R
@@ -12,7 +12,7 @@ library("gplots")
 
 
 # Choose the wd
-setwd("C:/Users/Jacob/Documents/Harrow 2020/Supplimental Lighting Experiments/Supplemental-Lighting-Git/Development/Openrefined data/From Kaye")
+setwd("C:/Users/Jacob/Documents/Harrow 2020/Supplimental Lighting Experiments/Supplemental-Lighting-Git/Development")
 #setwd("./Openrefined data") # "." refers to the file that this script is already in
 getwd()
 
@@ -21,27 +21,18 @@ getwd()
 # OpenRefine Lesson: http://datacarpentry.org/OpenRefine-ecology-lesson/
 
 # Development Rate
-Rate = tbl_df(read.csv("Development Rate Openrefined.csv", header = TRUE))
-head(Rate)
-summary(Rate)
-# Development Rate with Nymphal Instars in 'long' format
-Longrate = tbl_df(read.csv("Development Rate Openrefined Long.csv", header = TRUE))
+Dev = tbl_df(read.csv("Consolidated-Orius-Development-JB2020-Raw-Development-Openrefined.csv", header = TRUE))
+head(Dev)
 
-# % Mortality
-Mort = tbl_df(read.csv("Development Mortality Openrefined.csv", header = TRUE))
-head(Mort)
-summary(Mort)
+DevAdults = Dev %>% filter(Fate == "A")
 
 # Tibial Length
-Tibia = tbl_df(read.csv("Development Tibial Length Openrefined.csv", header = TRUE))
-head(Tibia)
-summary(Tibia)
 
 
 # Development Rate Analysis -----------------------------------------------
 
 # measures of spread
-summary(Rate)
+summary(Dev)
 
 # Transformations
   # Rate$Total.Development..days. = sqrt(Rate$Total.Development..days.)
@@ -49,88 +40,67 @@ summary(Rate)
   # Rate$Total.Development..days. = log(Rate$Total.Development..days.)
     # Log transformation results in nearly identical results to untransformed data
 
+# Summary stats for Development (Mean, SD, SE, n)
+Tdevsummary = DevAdults %>% group_by(Block, Treatment, Sex) %>% summarise(Mean = mean(TotalDevelopment), SD = sd(TotalDevelopment), SE = sd(TotalDevelopment)/sqrt(length(TotalDevelopment)), n = length(TotalDevelopment))
+Tdevsummary
+  # for Total and all Nymphal Instars (from https://dplyr.tidyverse.org/articles/colwise.html#multiple-functions-1trying, also https://dplyr.tidyverse.org/reference/group_by.html and https://community.rstudio.com/t/summarise-multiple-columns-using-multiple-functions-in-a-tidy-way/8645 )
+  summarystatfunctions = list(
+    Mean = ~ mean(.x), 
+    SD = ~ sd(.x), 
+    SE = ~ sd(.x)/sqrt(length(.x)), 
+    n = ~ length(.x)
+  )
+  Devsummaryallnymphalinstars = DevAdults %>% summarise(across(.cols = N1 : TotalDevelopment, .fns = summarystatfunctions, .names = "{col}.{fn}"))
+  Devsummaryallnymphalinstars
+  # or usea  loop
 
-
-# Histograms (as in adult Longevity)
+# Histograms 
 Treatmentlevelsorder = c("S", "W", "HPS", "HB", "HR", "LB", "LR")
-ggplot(Rate, aes(x = Total.Development..days., color = factor(Block), fill = factor(Block))) +
+ggplot(DevAdults, aes(x = TotalDevelopment, color = factor(Sex), fill = factor(Sex))) +
   geom_histogram() +
   xlab("Total Development (days)") +
   ylab("Frequency") +
-  facet_wrap(~factor(Treatment, levels = Treatmentlevelsorder), ncol = 3, scales = "fixed")
+  theme_classic() +
+  facet_wrap(~factor(Block) + factor(Treatment, levels = Treatmentlevelsorder), ncol = 3, scales = "fixed")
 # then for all Nymphal Instars
   # ? need nymphal instar in long form?
 
-# Summary stats (Mean, SD, SE, n)
-# just Total Development (days)
-Rate %>% summarise(Mean = mean(Total.Development..days.), SD = sd(Total.Development..days.), SE = sd(Total.Development..days.)/sqrt(length(Total.Development..days.)), n = length(Total.Development..days.))
-# then grouped by Block and Treatment
-Rate$Block = factor(Rate$Block) # redefine Block as a factor to not confuse certain functions
-Groupedrate = Rate %>% group_by(Block, Treatment)
-Groupedsummary = Groupedrate %>% summarise(Mean = mean(Total.Development..days.), SD = sd(Total.Development..days.), SE = sd(Total.Development..days.)/sqrt(length(Total.Development..days.)), n = length(Total.Development..days.))
-Groupedsummary
-# then for all Nymphal Instars (from https://dplyr.tidyverse.org/articles/colwise.html#multiple-functions-1trying, also https://dplyr.tidyverse.org/reference/group_by.html and https://community.rstudio.com/t/summarise-multiple-columns-using-multiple-functions-in-a-tidy-way/8645 )
-summarystatfunctions = list(
-  Mean = ~ mean(.x), 
-  SD = ~ sd(.x), 
-  SE = ~ sd(.x)/sqrt(length(.x)), 
-  n = ~ length(.x)
-)
-Groupedsummaryallnymphalinstars = Groupedrate %>% summarise(across(.cols = N1d : Total.Development..days., .fns = summarystatfunctions, .names = "{col}.{fn}"))
-Groupedsummaryallnymphalinstars
-
-# Boxplot
-boxplot(Rate$Total.Development..days. ~ Rate$Treatment, xlab = "Treatment", ylab = "Total Development (days)")
-# or using ggplot
-ggplot(Rate, aes(x= Treatment, y= Total.Development..days.)) + 
-  geom_boxplot() +
-  xlab("Treatment") +
-  ylab("Total Development (days)")
-
 # ? make Nymphal Instar Development Time into long format?
 
-# ANOVA
-TotaldevANOVA= aov(Rate$Total.Development..days. ~ Rate$Treatment)
-summary(TotaldevANOVA)
+# multi-ANOVA including interaction effects (from http://www.sthda.com/english/wiki/two-way-anova-test-in-r)
+TdevANOVA= aov(DevAdults$TotalDevelopment ~ DevAdults$Block + DevAdults$Treatment + DevAdults$Sex + DevAdults$Block:DevAdults$Treatment + DevAdults$Block:DevAdults$Sex + DevAdults$Treatment:DevAdults$Sex)
+summary(TdevANOVA)
+  # no significant difference between sexes so combined, and no interaction effects
 
 # Tukey HSD
-TotaldevTukey = HSD.test(TotaldevANOVA, trt = 'Rate$Treatment', group = TRUE)
-TotaldevTukey
+TdevTukeyTreatment = HSD.test(TdevANOVA, trt = 'DevAdults$Treatment', group = TRUE)
+TdevTukeyTreatment
 
-# is there a difference between Blocks? ie. use 2-way anova with Block and Treatment including interaction effect (from http://www.sthda.com/english/wiki/two-way-anova-test-in-r)
-Totaldev2wayANOVA = aov(Rate$Total.Development..days. ~ Rate$Block + Rate$Treatment + Rate$Block:Rate$Treatment)
-summary(Totaldev2wayANOVA)
-TotaldevTukeyBlock = HSD.test(Totaldev2wayANOVA, trt = 'Rate$Block', group = TRUE)
-TotaldevTukeyBlock
-TotaldevTukeyTreatment = HSD.test(Totaldev2wayANOVA, trt = 'Rate$Treatment', group = TRUE)
-TotaldevTukeyTreatment
-boxplot(Rate$Total.Development..days. ~ Rate$Block + Rate$Treatment, xlab = "Treatment", ylab = "Total Development (days)")
 # make a means plot to illustrate any interaction effect
 level_order <- c('W', 'S', 'HPS', 'HB', 'HR', 'LB', 'LR')
-meansplot = 
-  ggplot(data = Groupedsummary, aes(y = Mean, x = factor(Treatment, levels = level_order), group = Block, col = Block)) + # (from http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization)
-  geom_line() + # to colour code by Block, could add color = Groupedsummary$Block within geom_line() 
+Tdevmeansplot = 
+  ggplot(data = Tdevsummary, aes(y = Mean, x = factor(Treatment, levels = level_order), group = Block, col = Block)) + # (from http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization)
+  geom_line() + 
   geom_point() +
   ylab("Mean Total Development (days)")
   # geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE)) # to give error bars of +-SE
-meansplot
-# Yes there is a sig diff, but some Treatments don't having samples in both Blocks (W, LR)
+Tdevmeansplot
 
-# Boxplot with significant groups (as in Longevity)
-Grouplabels = tbl_df(TotaldevTukeyTreatment$groups["groups"])
-Grouplabels$TreatmentName = row.names(TotaldevTukeyTreatment$groups)
-Max = TotaldevTukeyTreatment$means["Max"]
-Maxtbl = as_tibble(Max, rownames = "TreatmentName")
-Grouplabels = merge(Grouplabels, Maxtbl, by = "TreatmentName")
+# Boxplot with significant groups
+Grouplabels = tbl_df(TdevTukeyTreatment$groups["groups"])
+Grouplabels$Treatment = row.names(TdevTukeyTreatment$groups)
+Max = TdevTukeyTreatment$means["Max"]
+Maxtbl = as_tibble(Max, rownames = "Treatment")
+Grouplabels = merge(Grouplabels, Maxtbl, by = "Treatment")
 Grouplabels$aboveMax = Grouplabels$Max + 1
 Grouplabels
 absMax = max(Grouplabels$Max)
 Treatmentlevelsorder = c("S", "W", "HPS", "HB", "HR", "LB", "LR")
-ggplot(Rate, aes(y = Total.Development..days., x = factor(Treatment, levels = Treatmentlevelsorder))) +
-  geom_boxplot(aes(color = Block)) + 
+ggplot(DevAdults, aes(y = TotalDevelopment, x = factor(Treatment, levels = Treatmentlevelsorder))) +
+  geom_boxplot(aes(color = factor(Block))) + 
   xlab("Treatment") +
   ylab("Total Development (days)") +
-  geom_text(data = Grouplabels, aes(x = TreatmentName, y = aboveMax, label = groups)) # apply the labels from the tibble
+  geom_text(data = Grouplabels, aes(x = Treatment, y = aboveMax, label = groups)) # apply the labels from the tibble
     # to put labels all at same height, y = absMax + absMax*0.05
 
 # Assumptions
@@ -141,17 +111,18 @@ ggplot(Rate, aes(y = Total.Development..days., x = factor(Treatment, levels = Tr
     # Histograms
     # Shapiro-Wilk or Kolmogorov-Smirnov (Lilliefors) tests
       library("rstatix")
-      Groupedrate %>% shapiro_test(Total.Development..days.) # from https://www.datanovia.com/en/lessons/normality-test-in-r/
+      DevAdults %>% group_by(Block, Sex) %>% shapiro_test(TotalDevelopment) # from https://www.datanovia.com/en/lessons/normality-test-in-r/
     # Q-Q Plots
-      ggplot(Rate, aes(sample = Total.Development..days., color = Block)) + # from https://ggplot2.tidyverse.org/reference/geom_qq.html or https://www.datanovia.com/en/lessons/ggplot-qq-plot/
+      ggplot(DevAdults, aes(sample = TotalDevelopment, color = factor(Block))) + # from https://ggplot2.tidyverse.org/reference/geom_qq.html or https://www.datanovia.com/en/lessons/ggplot-qq-plot/
         stat_qq() + stat_qq_line() +
         xlab("Theoretical") +
         ylab("Sample") +
         facet_wrap(~factor(Treatment, levels = Treatmentlevelsorder), ncol = 3, scales = "fixed")
   # equal variance across populations "homogeneity of variance" "homoscedasticity" 
     # Levene's test
-      leveneTest(Rate$Total.Development..days. ~ Rate$Treatment * factor(Rate$Block), center = median)
-      Rate %>% levene_test(formula = Total.Development..days. ~ Treatment * factor(Block), center = median) # from https://www.datanovia.com/en/lessons/homogeneity-of-variance-test-in-r/
+      # leveneTest(DevAdults$TotalDevelopment ~ DevAdults$Treatment * factor(DevAdults$Block), center = median)
+      DevAdults %>% levene_test(formula = TotalDevelopment ~ Treatment * factor(Block) * factor(Sex), center = median) # from https://www.datanovia.com/en/lessons/homogeneity-of-variance-test-in-r/
+        # ? does using all three x factors work?
 
 
 
